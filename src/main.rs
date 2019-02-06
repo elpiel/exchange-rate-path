f67b046be4758fd36502a5740e129f8f5b4f2bce
Lachezar Lechev <elpiel93@gmail.com>
2019-02-06 10:54:33 +0100

    [WIP] Graph - Trade - tests and partial non-working implementation

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,26 @@
 use std::io;
-use std::str::FromStr;
 
-use petgraph::graphmap::DiGraphMap;
-use petgraph::graphmap::GraphMap;
-
-use exchange_rate_path::command::PriceUpdate;
-use exchange_rate_path::graph::collection::IndexHashMap;
-use exchange_rate_path::{parse_line, ParseCommandError, ParsedLine};
-
-#[derive(Debug, PartialEq, Eq, Hash)]
-pub struct Exchange {
-    name: String,
-}
-
-impl Exchange {
-    pub fn new(exchange: String) -> Self {
-        Self { name: exchange }
-    }
-}
-
-#[derive(Debug, PartialEq, Eq, Hash)]
-pub struct Currency {
-    pub name: String,
-}
-
-impl Currency {
-    pub fn new(currency: String) -> Self {
-        Self { name: currency }
-    }
-}
-
-//struct ExchangeGraph<'a> {
-//    graph: DiGraphMap<(&'a str, &'a str), f64>
-//}
-
-type GraphType<'a> = DiGraphMap<usize, f64>;
+use exchange_rate_path::graph::handler::GraphHandler;
+use exchange_rate_path::{parse_line, ParsedLine};
 
 fn main() {
-    let _line = "2017-11-01T09:42:23+00:00 KRAKEN BTC USD 1000.0 0.0009";
-    let _line = "2017-11-01T09:42:23+00:00 EXCI USD BTC 0.0006 800.0";
+    let _case_1 = "2017-11-01T09:42:23+00:00 KRAKEN BTC USD 1000.0 0.0009";
+    let _case_1 = "2018-11-01T09:42:23+00:00 KRAKEN BTC USD 5000.0 3";
+    let _case_1 = "2017-11-01T09:42:23+00:00 EXCI USD BTC 0.0006 800.0";
+
+    // 0 - BTC 1 - USD 0-1: 1000 1-0: 0000.9
+    let _case_2 = "2017-11-01T09:42:23+00:00 KRAKEN BTC USD 1000.0 0.0009";
+    // 0 - BTC 2 - EUR 0-2: 5000 2-0: 3.0
+    let _case_2 = "2018-11-01T09:42:23+00:00 KRAKEN BTC EUR 5000.0 3.0";
+    // 3 - USD 4 - BTC 3-4: 0.0006 4-3: 800 | 0-4: 1 1-3: 1 4-0: 1 0-4: 1
+    let _case_2 = "2017-11-01T09:42:23+00:00 EXCI USD BTC 0.0006 800.0";
+
     let _line = "EXCHANGE_RATE_REQUEST KRAKEN BTC EXCI USD";
 
     println!("The Exchange rate path problem solver");
-    let mut graph: GraphType = GraphMap::new();
-    let mut index_map: IndexHashMap<(String, String)> = IndexHashMap::new();
+    let mut handler = GraphHandler::default();
 
-    let mut read_lines: Vec<ParsedLine> = Vec::new();
-
-    for _ in 0..2 {
+    for _ in 0..3 {
         println!("Enter a command:");
 
         let mut command_line_str = String::new();
@@ -68,46 +41,22 @@ fn main() {
             }
         };
 
-        handle_command(&mut graph, &mut index_map, parsed_line);
+        handle_command(&mut handler, parsed_line);
+        for edge in handler.exchange_graph.graph.all_edges() {
+            dbg!(edge);
+        }
     }
 
-    println!("{:?}", index_map);
-    println!("{:?}", graph);
+    //    for edge in handler.graph.graph.all_edges() {
+    //        dbg!(edge);
+    //    }
 }
 
-fn add_price_update<'a>(
-    graph: &'a mut GraphType<'a>,
-    index_map: &mut IndexHashMap<(String, String)>,
-    price_update: PriceUpdate,
-) {
-    let source_node = (
-        price_update.exchange.clone(),
-        price_update.source_currency.clone(),
-    );
-    let dest_node = (
-        price_update.exchange.clone(),
-        price_update.destination_currency.clone(),
-    );
-
-    let source_node_index = index_map.entry(source_node);
-    let dest_node_index = index_map.entry(dest_node);
-
-    let source_node = graph.add_node(source_node_index);
-    let dest_node = graph.add_node(dest_node_index);
-
-    let source_to_dest = graph.add_edge(source_node, dest_node, price_update.forward_factor);
-    let source_to_dest = graph.add_edge(dest_node, source_node, price_update.backward_factor);
-}
-
-fn handle_command<'a>(
-    graph: &mut GraphType<'a>,
-    index_map: &mut IndexHashMap<(String, String)>,
-    parsed_line: ParsedLine,
-) {
+fn handle_command(graph_handler: &mut GraphHandler, parsed_line: ParsedLine) {
     match parsed_line {
         ParsedLine::PriceUpdate(price_update) => {
-            add_price_update(graph, index_map, price_update);
+            graph_handler.handle_update(price_update);
         }
-        ParsedLine::ExchangeRequest(exchange_request) => {}
+        ParsedLine::ExchangeRequest(_exchange_request) => {}
     }
 }
