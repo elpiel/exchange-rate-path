563b2ba53f833512010e4b4663b9d96023b1d357
Lachezar Lechev <elpiel93@gmail.com>
2019-03-04 22:02:38 +0100

    lib.rs - fix fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 pub use self::parse::command;
 pub use self::parse::parsing::{parse_line, ParseCommandError, ParseErrorKind, ParsedLine};
 
+pub mod display;
 pub mod graph;
 pub mod parse;
-pub mod display;
